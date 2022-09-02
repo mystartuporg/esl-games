8ae7dd09294117e7aa9eada85b235ff6555be8d5
Renzo Ralph Pua <35352850+renzoralphpua@users.noreply.github.com>
2022-09-03 02:20:25 +0800

    feat(basketball-game): added rewrite and redirect
    - this will ensure that `api/basketball` will be redirected and rendered to `basketball/play`

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  redirects: async () => [
+    {
+      source: "/api/basketball",
+      destination: "/basketball/play",
+      permanent: false
+    },
+  ],
+  rewrites: async () => [
+    {
+      source: "/basketball/play",
+      destination: "/api/basketball",
+    }
+  ]
 }
 
 module.exports = nextConfig
