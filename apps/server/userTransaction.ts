2ee8aa7b2b992228bee3a4669eca757ed23a418a
ValenArius <walterjannpura@gmail.com>
2022-10-12 23:33:19 +0800

    refactor: inserted query in try catch block and changed var to let

--- apps/server/userTransaction.ts
+++ apps/server/userTransaction.ts
@@ -18,30 +18,38 @@ interface UserTran {
 
 export const userTransaction: APIGatewayProxyHandler = async (event) => {
 
-    var body = JSON.parse(event.body ?? "") as UserTran
+    try {
+        let body = JSON.parse(event.body ?? "") as UserTran
 
-    var currentDate = new Date()
-    var tomorrow = new Date()
-    tomorrow.setDate(currentDate.getDate()+1)
-    tomorrow.setHours(0, 0, 0, 0)
-    var today = new Date(currentDate).setHours(0, 0, 0, 0)
-    var year = currentDate.getFullYear()
-    var month = currentDate.getMonth() + 1
-    var day = currentDate.getDate()
-    var refNumDate = year.toString() + month.toString().padStart(2, '0') + day.toString()
+        let currentDate = new Date()
+        let tomorrow = new Date()
+        tomorrow.setDate(currentDate.getDate()+1)
+        tomorrow.setHours(0, 0, 0, 0)
+        let today = new Date(currentDate).setHours(0, 0, 0, 0)
+        let year = currentDate.getFullYear()
+        let month = currentDate.getMonth() + 1
+        let day = currentDate.getDate()
+        let refNumDate = year.toString() + month.toString().padStart(2, '0') + day.toString()
 
-    var userCount: {count: number}[] = await mysql.query('SELECT COUNT(id) + 1 as count FROM transactions WHERE date BETWEEN ? and ?', [today, tomorrow])
+        let userCount: {count: number}[] = await mysql.query('SELECT COUNT(id) + 1 as count FROM transactions WHERE date BETWEEN ? and ?', [today, tomorrow])
 
-    var refNumID = userCount[0].count.toString().padStart(3, '0')
+        let refNumID = userCount[0].count.toString().padStart(3, '0')
 
-    var refNum = refNumDate + '-' + refNumID
+        let refNum = refNumDate + '-' + refNumID
 
-    await mysql.query('INSERT INTO transactions (reference_no, user_id, reward_id, date) VALUES (?, ?, ?, ?)', [refNum, body.user_id, body.reward_id, currentDate])
+        await mysql.query('INSERT INTO transactions (reference_no, user_id, reward_id, date) VALUES (?, ?, ?, ?)', [refNum, body.user_id, body.reward_id, currentDate])
 
-    await mysql.end()
+        await mysql.end()
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({referenceNumber: refNum})
+        return {
+            statusCode: 200,
+            body: JSON.stringify({referenceNumber: refNum})
+        }
+    }
+    catch (error){
+        return {
+            statusCode: 500,
+            body: JSON.stringify({message: error})
+        }
     }
 }
